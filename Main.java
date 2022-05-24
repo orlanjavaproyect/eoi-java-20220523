33699d28f1c99d3c2beb2527d33a37ea997166c7
orlan <orlandoalba81@gmail.com>
2022-05-24 03:37:26 +0200

    Creación Main

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hola Mundo");
-=======
         Counter.ImprimeNumeros();
->>>>>>> counter_if
     }
 }
